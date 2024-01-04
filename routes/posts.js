7c2ea6335941c759408cf1b559e4767fda3aef10
Elliot Akande <elbo.lynch@gmail.com>
2024-01-04 13:31:42 +0000

    Add Comments router

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/postsController");
+const commentsRouter = require("./comments");
 
 /* CREATE a post. */
 router.post("/", postsController.post_create);
 
 /* READ all posts. */
 router.get("/", postsController.post_list);
+
+/* ROUTER for comments from specified post */
+router.use("/:id/comments", commentsRouter);
 
 /* READ specified post. */
 router.get("/:id", postsController.post_detail);
