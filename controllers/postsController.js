5f1fba30765c53236dbbee926237fece62a618e6
Elliot Akande <elbo.lynch@gmail.com>
2024-01-04 15:32:07 +0000

    Implement Post Create route controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,16 +1,44 @@
 const Post = require("../models/post");
+const Author = require("../models/author");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Hande Post creation on POST.
-exports.post_create = asyncHandler(async (req, res, next) => {
-  res.send({ message: "Not yet implemented" });
-});
+exports.post_create = [
+  body("title", "Title must be present").trim().isLength({ min: 1 }).escape(),
+  body("content", "Post Content must be present")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("author", "Author ID must be present")
+    .trim()
+    .isLength({ min: 1 })
+    .custom(async (value) => {
+      const author = await Author.findById(value);
+      if (!author) throw new Error("Invalid Author ID");
+    }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors.array());
+      return;
+    }
+
+    const post = new Post({
+      author: req.body.author,
+      title: req.body.title,
+      content: req.body.content,
+      timestamp: new Date(),
+    });
+    post.save();
+    res.send(post);
+  }),
+];
 
 // Send list of all published Posts on GET.
 exports.post_list = asyncHandler(async (req, res, next) => {
-  const allPosts = await Post.find({ isPublished: true })
-    .populate("author", "username")
-    .exec();
+  const allPosts = await Post.find().populate("author", "username").exec();
 
   res.send(allPosts);
 });
