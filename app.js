cbb5362988c5e0ff0ee1762ea5358d57d27456dd
Elliot Akande <elbo.lynch@gmail.com>
2024-01-10 15:37:50 +0000

    Configure passport local strategy

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const Author = require("./models/author");
 
 const postsRouter = require("./routes/posts");
 
@@ -15,6 +19,25 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
+
+// Set up passport
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
 
 const app = express();
 
