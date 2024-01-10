55c6255d4172e70d18002f4577a3c99768298e41
Elliot Akande <elbo.lynch@gmail.com>
2024-01-10 15:38:27 +0000

    Add password constraints

--- models/author.js
+++ models/author.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, minLength: 8 },
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
