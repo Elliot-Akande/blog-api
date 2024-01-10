1b2a87d24fcea27d0b1cc4f70fb5186021e1048c
Elliot Akande <elbo.lynch@gmail.com>
2024-01-10 18:18:04 +0000

    Update username constraints

--- models/author.js
+++ models/author.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true, minLength: 8 },
 });
 
