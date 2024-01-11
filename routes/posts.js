507cfd71b56d2929349b7da463b319ae22a119a7
Elliot Akande <elbo.lynch@gmail.com>
2024-01-11 13:19:14 +0000

    Add authentication to routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,23 +2,36 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/postsController");
 const commentsRouter = require("./comments");
+const passport = require("passport");
 
-/* CREATE a post. */
-router.post("/", postsController.post_create);
+// CREATE a Post (Requires auth).
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  postsController.post_create
+);
 
-/* READ all posts. */
-router.get("/", postsController.post_list);
+// READ all published Posts.
+router.get("/public", postsController.post_list);
 
-/* ROUTER for comments from specified post */
+// ROUTER for comments from specified Post
 router.use("/:id/comments", commentsRouter);
 
-/* READ specified post. */
+// READ specified Post.
 router.get("/:id", postsController.post_detail);
 
-/* UPDATE specified post. */
-router.put("/:id", postsController.post_update);
+// UPDATE specified Post.
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postsController.post_update
+);
 
-/* DELETE specified post. */
-router.delete("/:id", postsController.post_delete);
+// DELETE specified Post.
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postsController.post_delete
+);
 
 module.exports = router;
