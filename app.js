4a0dd9f06fa73a6a0b491d333dade156cef8e98e
Elliot Akande <elbo.lynch@gmail.com>
2024-01-11 13:20:07 +0000

    Update error handler

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.send(JSON.stringify(err.message));
+  res.json({ msg: err.message });
 });
 
 module.exports = app;
